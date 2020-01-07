79fbc3828150d80536fe5cba2c4cfa3c13973db6
Karan Kumar <kumarkay.ca@gmail.com>
2020-01-07 16:08:11 -0500

    init

--- Ios_Sound/ViewController.swift
+++ Ios_Sound/ViewController.swift
@@ -7,13 +7,61 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
+    
+    
+    var player = AVAudioPlayer()
+    
+    let path=Bundle.main.path(forResource: "ck", ofType: "mp3")
+    
+    
+    var timer = Timer()
+    
+    
 
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    @IBOutlet weak var trackSeek: UISlider!
+    
+    @IBAction func stopBtn(_ sender: UIButton) {
+        player.stop()
+    }
+
+   
+    @IBAction func pauseBtn(_ sender: UIBarButtonItem) {
+        player.pause()
+    }
+    @objc func updateScrubber()
+    {
+      trackSeek.value=Float(player.currentTime)
+    }
+    
+    @IBAction func playBtn(_ sender: UIBarButtonItem) {
+        player.play()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:
+            #selector(updateScrubber), userInfo: nil, repeats: true)
+    }
+    
+    
+    
+    @IBAction func changeVolume(_ sender: UISlider) {
+        player.volume=volumeSlider.value
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        do{
+            player = try AVAudioPlayer(contentsOf:URL(fileURLWithPath: path!))
+        }
+        catch{
+            print(error)
+        }
+        
+        }
 
 
 }
